1c93f3a65a19804fb8f6625ba5803cc5288b7203
YuvaRaj Raghunapu <mdm.yuvaraj@gmail.com>
2020-03-03 11:13:13 -0500

    Updated with the datatypes

--- Classes/Python_001/RevisionFolder/March3.py
+++ Classes/Python_001/RevisionFolder/March3.py
@@ -5,15 +5,24 @@ March:03-2020 :
 
 '''
 
-
+#data types
 
 def main():
     name='yuvaraj'
     names=['yuvaraj','raghunapu']
     namePairs={'yuvaraj':'Mahathii'}
     nameDictionary=('Super','Machi')
-    print(name)
-    name.
+    rawname= b'super'
+    setName=set([3,4,56,6])
 
+
+    print(name)
+    print(name.isalnum())
+    print(type(name))
+    print(type(names))
+    print(type(namePairs))
+    print(type(nameDictionary))
+    print(type(rawname))
+    print(type(setName))
 if __name__=='__main__':
     main()
