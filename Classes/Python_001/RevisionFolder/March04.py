20fe4e80f8080d92d409ef2e7163b70ca2e01b18
YuvaRaj Raghunapu <mdm.yuvaraj@gmail.com>
2020-03-04 10:32:13 -0500

    simple update

--- Classes/Python_001/RevisionFolder/March04.py
+++ Classes/Python_001/RevisionFolder/March04.py
@@ -1,2 +1,19 @@
 # March04
-# updates 
+# updates
+
+# using the blackslash
+
+def main():
+    print('Simple start print')
+    #example of the 'backslash'
+
+    a =  1 \
+        +1 \
+        +5 \
+        +6
+
+    print('The total can be %d : ' %(a))
+
+
+if __name__ == '__main__':
+    main()
